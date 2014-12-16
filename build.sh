b72766fc351ddf887309a1702f02e41b689a0296
Volumio <info@volumio.org>
2014-12-16 17:48:00 +0100

    First Commit

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#Volumio Image Builder
+#
+#
+echo "Executing Multistrap"
+echo "Building Base Jessie System"
+multistrap -a armhf -f conf/minimal.conf
+cp /usr/bin/qemu-arm-static build/root/usr/bin/
+cp firstconfig.sh build/root
+mount /dev build/root/dev -o bind
+mount /proc build/root/proc -t proc
+mount /sys build/root/sys -t sysfs
+chroot build/root /bin/bash -x <<'EOF'
+su -
+./firstconfig.sh
+
+
---
+++ firstconfig.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This script will be run in chroot under qemu.
+
+export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
+export LC_ALL=C LANGUAGE=C LANG=C
+/var/lib/dpkg/info/dash.preinst install
+dpkg --configure -a
+
+groupadd volumio
+useradd -c volumio -d /home/volumio -m -g volumio -G adm,dialout,cdrom,floppy,audio,dip,video,plugdev -s /bin/bash -p '$6$ZZZZZZZZ$WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW' volumio
+
+
+
+echo "volumio ALL=(ALL) ALL" >> /etc/sudoers
+chown -R volumio: /home/volumio/.ssh
+chmod -R go= /home/volumio/.ssh
+
+
+cat > /etc/network/interfaces << EOF
+# interfaces(5) file used by ifup(8) and ifdown(8)
+auto lo
+iface lo inet loopback
+
+allow-hotplug eth0
+iface eth0 inet dhcp
+
+allow-hotplug wlan0
+
+EOF
+chmod 600 /etc/network/interfaces
+
+echo volumio > /etc/hostname
+
+
+# cleanup
+apt-get clean
+rm /etc/resolv.conf
+rm -rf /tmp/*
+rm /configscript.sh /usr/bin/qemu-arm-static
