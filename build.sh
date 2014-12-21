e3a32f1cfeec496b83188946810172f58e3066d2
root <root@VolumioDevBox.VolumioDevBox>
2014-12-21 17:42:55 -0500

    Added write to image

--- build.sh
+++ build.sh
@@ -1,7 +1,59 @@
-#!/bin/sh
+#!/bin/bash
 #Volumio Image Builder
 #
 #
+
+#Initialize variables to default values.
+BUILD=false
+
+#Set fonts for Help.
+NORM=`tput sgr0`
+BOLD=`tput bold`
+REV=`tput smso`
+
+#Help function
+function HELP {
+  echo -e \\n"Help documentation for Volumio Image Builder"\\n
+  echo -e "Basic usage: ./build.sh -b -d all -v 2.0"\\n
+  echo "Switches:"
+  echo "-b      --Build system with Multistrap, optional but required upon first usage"
+  echo "-d      --Create Image for Specific Devices. Usage: all (all), pi, udoo, cuboxi, bbb, cubietruck, compulab"
+  echo "-v      --Version"
+  echo -e "Example: Build a Raspberry PI image from scratch, version 2.0 : ./build.sh -b -d pi -v 2.0 "\\n
+  exit 1
+}
+
+#Check the number of arguments. If none are passed, print help and exit.
+NUMARGS=$#
+if [ $NUMARGS -eq 0 ]; then
+  HELP
+fi
+
+while getopts :v:d:be FLAG; do
+  case $FLAG in
+    b)  
+      BUILD=true
+      ;;
+    d) 
+      DEVICE=$OPTARG
+      ;;
+    v)
+      VERSION=$OPTARG
+      ;;
+    h)  #show help
+      HELP
+      ;;
+    /?) #unrecognized option - show help
+      echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
+      HELP
+      ;;
+  esac
+done
+
+shift $((OPTIND-1)) 
+
+if [ "$BUILD" = true ]; then
+echo "Building $VERSION base system"
 echo "Executing Multistrap"
 echo "Building Base Jessie System"
 mkdir build
@@ -22,4 +74,10 @@ echo "Unmounting Temp devices"
 umount -l build/root/dev 
 umount -l build/root/proc 
 umount -l build/root/sys 
+sh scripts/configure.sh
 
+else 
+echo 'Writing UDOO Image File'
+sh scripts/udooimage.sh -v 1.5 -d UDOO
+
+ 
