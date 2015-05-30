34feee16cc65c28ff868bcecc8566b82716d9acc
volumio <info@volumio.org>
2015-05-30 16:48:44 -0400

    FStab improvements

--- scripts/raspberryconfig.sh
+++ scripts/raspberryconfig.sh
@@ -6,9 +6,9 @@ echo "Creating Fstab File"
 
 touch /etc/fstab
 echo "proc            /proc           proc    defaults        0       0
-/dev/mmcblk0p1  /boot           vfat    defaults        0       0
-/dev/mmcblk0p2  /               ext4    defaults,noatime  0   1
-tmpfs   /var/log                tmpfs   size=20M,defaults,noatime,mode=0755 0 0 
+/dev/mmcblk0p1  /boot           vfat    defaults,utf8,user,rw,umask=111,dmask=000        0       1
+/dev/mmcblk0p2  /               ext4    defaults,noatime,nodiratime  0   2
+tmpfs   /var/log                tmpfs   size=20M,nodev,uid=1000,mode=0777,gid=4, 0 0 
 tmpfs   /var/cache/apt/archives tmpfs   size=100M,defaults,noexec,nosuid,nodev,mode=0755 0 0
 tmpfs   /var/spool/cups         tmpfs   size=100M,defaults,noatime,mode=0755 0 0
 tmpfs   /var/spool/cups/tmp     tmpfs   defaults,noatime,mode=0755 0 0
