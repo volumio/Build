563eb0575879df3833368544a951ebf3e063332a
volumio <info@volumio.org>
2015-07-31 22:28:35 -0400

    Make me a sandwich

--- scripts/volumioconfig.sh
+++ scripts/volumioconfig.sh
@@ -50,6 +50,8 @@ alias updatey="sudo apt-get --yes"
 ## Read Like humans ##
 alias df='df -H'
 alias du='du -ch'
+alias make me a sandwich='echo "What? Make it yourself"'
+alias sudo make me a sandwich='echo "OKAY"'
 ' >> /etc/bash.bashrc
 
 #Sudoers Nopasswd
