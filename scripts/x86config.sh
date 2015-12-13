5c60cecc8bc5fca260934b245de0cd2c3caea0bc
gkkpch <ge.koerkamp@gmail.com>
2015-12-13 16:27:28 +0100

    added busybox, fixes missing '/bin/touch' in initrd

--- scripts/x86config.sh
+++ scripts/x86config.sh
@@ -9,9 +9,8 @@ echo "Creating symlinks to stay compatible with the armv6/v7 platforms"
 ln -s /usr/bin/node /usr/local/bin/node
 ln -s /usr/bin/nodejs /usr/local/bin/nodejs
 
-#TODO temporary mpd.conf fix, to be moved to a patch file...
+#TODO temporary mpd.conf fix and s/pdif unmute, to be moved to a patch file...
 sed -i "s/hw:0,0/hw:0,1/g" /etc/mpd.conf
-
  
 echo "Create grub config folder"
 mkdir -p /boot/grub
@@ -29,7 +28,9 @@ rpl -ivRd -x'.cfg' `echo root="$( echo ${LOOP_PART})"` `echo root=UUID="$( echo 
 echo "Editing fstab to use UUID"
 sed -i "s/\/dev\/sda1/`echo UUID="$( echo ${UUID})"`/g" /etc/fstab
 
-echo "Updating initramfs to avound fsck errors"
+echo "Updating initramfs to avound fsck errors, also needs busybox"
+apt-get update
+apt-get -y install busybox
 VERSION="$(ls -t /lib/modules | cat | head -n2)"
 update-initramfs -u -k $VERSION
 
