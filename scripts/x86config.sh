841665d59ef23187a44a354eb5d5ed7db7e620cf
gkkpch <ge.koerkamp@gmail.com>
2015-12-18 13:56:19 +0100

    replace grub2 by lilo

--- scripts/x86config.sh
+++ scripts/x86config.sh
@@ -5,33 +5,42 @@
 echo "Initialising.."
 . init.sh
 
-echo "Creating symlinks to stay compatible with the armv6/v7 platforms"
+echo "Creating node/ nodejs symlinks to stay compatible with the armv6/v7 platforms"
 ln -s /usr/bin/node /usr/local/bin/node
 ln -s /usr/bin/nodejs /usr/local/bin/nodejs
 
 #TODO temporary mpd.conf fix and s/pdif unmute, to be moved to a patch file...
 sed -i "s/hw:0,0/hw:0,1/g" /etc/mpd.conf
- 
-echo "Create grub config folder"
-mkdir -p /boot/grub
 
-echo "Applying Grub Configuration"
-grub-mkconfig -o /boot/grub/grub.cfg
+#echo "Create grub config folder"
+#mkdir -p /boot/grub
 
-echo "Installing grub bootloader"
-update-grub
-grub-install --boot-directory=/boot $LOOP_DEV
+#echo "Applying Grub Configuration"
+#grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "Fixing Grub Boot device"
-rpl -ivRd -x'.cfg' `echo root="$( echo ${LOOP_PART})"` `echo root=UUID="$( echo ${UUID})"` /boot/grub 
+#echo "Installing grub bootloader"
+#update-grub
+#grub-install --boot-directory=/boot $LOOP_DEV
+
+#echo "Fixing Grub Boot device"
+#rpl -ivRd -x'.cfg' `echo root="$( echo ${LOOP_PART})"` `echo root=UUID="$( echo ${UUID})"` /boot/grub 
 
 echo "Editing fstab to use UUID"
 sed -i "s/\/dev\/sda1/`echo UUID="$( echo ${UUID})"`/g" /etc/fstab
 
-echo "Updating initramfs to avound fsck errors, also needs busybox"
-apt-get update
-apt-get -y install busybox
 VERSION="$(ls -t /lib/modules | cat | head -n2)"
+
+echo "Preparing lilo.conf"
+sed -i "s/%%UUID%%/`echo ${UUID}`/g" /etc/lilo.conf
+sed -i "s~%%BOOTPART%%~`echo ${LOOP_DEV}`~g" /etc/lilo.conf
+sed -i "s/%%VERSION%%/`echo ${VERSION}`/g" /etc/lilo.conf
+
+#echo "Updating initramfs to avound fsck errors, also needs busybox"
+#apt-get update
+#apt-get -y install busybox
+#TODO: it seems that update-initramfs also takes care of lilo
+#TODO: Check if 'lilo' can be omitted.
 update-initramfs -u -k $VERSION
+lilo
 
 echo "Bootloader configuration complete"
