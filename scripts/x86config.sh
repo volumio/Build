741d2aed785c1588dc123f5270b1242c073a4f5d
gkkpch <ge.koerkamp@gmail.com>
2015-12-21 15:52:19 +0100

    small additions to grub2 install

--- scripts/x86config.sh
+++ scripts/x86config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # This script will be run in chroot.
 echo "Initialising.."
 . init.sh
@@ -9,20 +8,17 @@ echo "Creating node/ nodejs symlinks to stay compatible with the armv6/v7 platfo
 ln -s /usr/bin/node /usr/local/bin/node
 ln -s /usr/bin/nodejs /usr/local/bin/nodejs
 
-#TODO temporary mpd.conf fix and s/pdif unmute, to be moved to a patch file...
-sed -i "s/hw:0,0/hw:0,1/g" /etc/mpd.conf
-
 # Make grub boot menu transparent
 sed -i "s/menu_color_normal=cyan\/blue/menu_color_normal=white\/black/g" /etc/grub.d/05_debian_theme
+sed -i "s/menu_color_highlight=white\/blue/menu_color_highlight=green\/dark-gray/g" /etc/grub.d/05_debian_theme
 
-#echo "Create grub config folder"
+#echo "Creating grub config folder"
 mkdir -p /boot/grub
 
 echo "Applying Grub Configuration"
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "Installing grub bootloader"
-update-grub
 grub-install --boot-directory=/boot $LOOP_DEV
 
 echo "Fixing root and boot device in case loop device was used"
@@ -31,7 +27,7 @@ rpl -ivRd -x'.cfg' `echo root="$( echo ${LOOP_PART})"` `echo root=UUID="$( echo 
 echo "Editing fstab to use UUID"
 sed -i "s/\/dev\/sda1/`echo UUID="$( echo ${UUID})"`/g" /etc/fstab
 
-#echo "Updating initramfs to avound fsck errors, also needs busybox"
+#echo "Updating initramfs to avound fsck errors"
 VERSION="$(ls -t /lib/modules | cat | head -n2)"
 update-initramfs -u -k $VERSION
 
