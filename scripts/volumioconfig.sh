5c93862e8237f30b0db2edbf72b79c9bc9a5b48f
Volumio <info@volumio.org>
2016-01-23 19:19:17 +0100

    Proper Network interfaces for wireless

--- scripts/volumioconfig.sh
+++ scripts/volumioconfig.sh
@@ -70,9 +70,12 @@ cat > /etc/network/interfaces << EOF
 auto lo
 iface lo inet loopback
 
-auto eth0
+allow-hotplug eth0
 iface eth0 inet dhcp
 
+auto wlan0
+iface wlan0 inet dhcp
+ wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
 EOF
 chmod 666 /etc/network/interfaces
 
