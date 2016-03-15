2315e3c07442b4f58400389f589c6fd2a5b458e0
Gé Koerkamp <ge.koerkamp@gmail.com>
2016-03-15 22:26:03 +0100

    add loadable sound modules for C2 only

--- scripts/odroidcconfig.sh
+++ scripts/odroidcconfig.sh
@@ -17,13 +17,15 @@ tmpfs   /tmp                    tmpfs   defaults,noatime,mode=0755 0 0
 tmpfs   /dev/shm                tmpfs   defaults        0 0
 " > /etc/fstab
 
-
-echo "Adding sound modules"
-#TODO: pcm5102 is needed, snd_soc_odroid_dac could be aded runtime (config?)
-echo "
+  
+if [ -e "/c2.flag" ]; then
+  echo "Adding sound modules"
+  #TODO: only works for C2 at the moment
+  echo "
 snd_soc_pcm5102
 snd_soc_odroid_dac
 " >> /etc/modules
+fi
 
 echo "Prevent services starting during install, running under chroot" 
 echo "(avoids unnecessary errors)"
