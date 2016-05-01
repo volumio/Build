95505e2d4535ebca9ab24c7fdf888884515d8fc5
Gé Koerkamp <ge.koerkamp@gmail.com>
2016-05-01 15:53:30 +0200

    build script improvement for cases where the rootfs is used for different devices

--- build.sh
+++ build.sh
@@ -35,8 +35,8 @@ function check_os_release {
   DEVICE=$3
 
   if [ "$HAS_VERSION" -ne "0" ]; then
-	# os-release already has a VERSION number
-    # cut the last 2 lines in other devices are being built from the same rootfs
+    # os-release already has a VERSION number
+    # cut the last 2 lines in case other devices are being built from the same rootfs
 	head -n -2 build/${ARCH_BUILD}/root/etc/os-release > build/${ARCH_BUILD}/root/etc/tmp-release
 	mv build/${ARCH_BUILD}/root/etc/tmp-release build/${ARCH_BUILD}/root/etc/os-release
   fi
