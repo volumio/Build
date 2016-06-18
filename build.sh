139196ab6bb4a0894909c99cf16704f03d5caa3c
Volumio <info@volumio.org>
2016-06-18 16:22:02 +0100

    depth 1 in cloning

--- build.sh
+++ build.sh
@@ -109,7 +109,7 @@ if [ -n "$BUILD" ]; then
 
   echo 'Cloning Volumio Node Backend'
   mkdir build/$BUILD/root/volumio
-  git clone -b dev https://github.com/volumio/Volumio2.git build/$BUILD/root/volumio
+  git clone --depth 1 --branch dev https://github.com/volumio/Volumio2.git build/$BUILD/root/volumio
 
   echo 'Cloning Volumio UI'
   #git clone -b dist --single-branch https://github.com/volumio/Volumio2-UI.git build/$BUILD/root/volumio/http/www
