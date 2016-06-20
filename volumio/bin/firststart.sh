457c46eaca94fc77991817e81282d215cea79868
Volumio <info@volumio.org>
2016-06-20 19:41:29 +0200

    no download for winbind

--- volumio/bin/firststart.sh
+++ volumio/bin/firststart.sh
@@ -9,8 +9,6 @@ echo "Installing winbind, its done here because else it will freeze networking"
 
 mkdir /var/log/samba
 cd /
-wget http://repo.volumio.org/Volumio2/Binaries/arm/libnss-winbind_23a4.2.10+dfsg-0+deb8u3_armhf.deb
-wget http://repo.volumio.org/Volumio2/Binaries/arm/winbind_23a4.2.10+dfsg-0+deb8u3_armhf.deb
 dpkg -i libnss-winbind_23a4.2.10+dfsg-0+deb8u3_armhf.deb
 dpkg -i winbind_23a4.2.10+dfsg-0+deb8u3_armhf.deb
 rm /libnss-winbind_23a4.2.10+dfsg-0+deb8u3_armhf.deb
