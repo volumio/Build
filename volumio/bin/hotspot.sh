76e3d6c3ba65b5919185c9f3026e58944c22a5fd
Volumio <info@volumio.org>
2016-07-12 00:03:49 +0200

    Update hotspot.sh

--- volumio/bin/hotspot.sh
+++ volumio/bin/hotspot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+case "$1" in
+'start')
 DRIVER=`/sbin/ethtool -i wlan0 | grep driver | awk -F": " '{print $2}'`
 
 if [ $DRIVER = "rtl8192cu" ] ; then
@@ -9,3 +11,13 @@ else
   echo "LaunghingOrdinary Hostapd"
 /usr/sbin/hostapd /etc/hostapd/hostapd.conf
 fi
+;;
+'stop')
+
+echo "Killing Hostapd"
+/usr/bin/sudo/ /usr/bin/killall hostapd
+
+echo "Killing Dhcpd"
+/usr/bin/sudo /usr/bin/killall dhcpd
+;;
+esac
