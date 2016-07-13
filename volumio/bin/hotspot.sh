ee9c330f238825bfdfb44e3a1dcdf207cb588b6d
Volumio <info@volumio.org>
2016-07-13 19:19:09 +0200

    Different conf for edimax

--- volumio/bin/hotspot.sh
+++ volumio/bin/hotspot.sh
@@ -6,7 +6,7 @@ DRIVER=`/sbin/ethtool -i wlan0 | grep driver | awk -F": " '{print $2}'`
 
 if [ $DRIVER = "rtl8192cu" ] ; then
   echo "Launching Hostapd Edimax"
-/usr/sbin/hostapd-edimax /etc/hostapd/hostapd.conf
+/usr/sbin/hostapd-edimax /etc/hostapd/hostapd-edimax.conf
 else
   echo "LaunghingOrdinary Hostapd"
 /usr/sbin/hostapd /etc/hostapd/hostapd.conf
