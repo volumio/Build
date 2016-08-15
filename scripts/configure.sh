4058bcf185926c431b113c6cea43a74d0701d29e
Volumio <info@volumio.org>
2016-08-16 00:14:29 +0200

    Fixed  copy of dynswap.sh

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -74,6 +74,8 @@ cp volumio/etc/nsswitch.conf build/$BUILD/root/etc/nsswitch.conf
 cp volumio/bin/firststart.sh build/$BUILD/root/bin/firststart.sh
 #hotspot
 cp volumio/bin/hotspot.sh build/$BUILD/root/bin/hotspot.sh
+#dynswap
+cp volumio/bin/dynswap.sh build/$BUILD/root/bin/dynswap.sh
 #Wireless
 cp volumio/bin/wireless.js build/$BUILD/root/volumio/app/plugins/system_controller/network/wireless.js
 echo 'Done Copying Custom Volumio System Files'
