96178ac03d9c349729277bcfef48cd48ddd10018
Gé Koerkamp <ge.koerkamp@gmail.com>
2016-08-22 22:57:51 +0200

    Solving backup GPT table move at first boot

--- scripts/x86config.sh
+++ scripts/x86config.sh
@@ -32,7 +32,7 @@ echo "  Creating syslinux.cfg template for Syslinux Legacy BIOS"
 echo "DEFAULT volumio
 
 LABEL volumio
-  SAY Legacy Boot Volumio Audiophile Music Player (default)
+  SAY Legacy Boot Volumio Audiophole Music Player (default)
   LINUX ${KRNL}
   APPEND ro imgpart=LABEL=volumioimg bootpart=LABEL=volumioboot imgfile=volumio_current.sqsh quiet splash ${DEBUG}
   INITRD volumio.initrd
@@ -167,9 +167,8 @@ echo "  No need for the system map either"
 DELFILE=`ls -l /boot |grep System.map | awk '{print $9}'`
 echo "  Found "$DELFILE", deleting"
 rm /boot/${DELFILE}
-ls -l /boot
 
-echo "  Signalling the init script to move the GPT backup partition table and re-size the volumio data partition"
+echo "  Signalling the init script to re-size the volumio data partition"
 touch /boot/resize-volumio-datapart
 
 echo "Bootloader configuration and initrd.img complete"
