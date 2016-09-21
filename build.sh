c90f6c857559049bfc5bf2c6887ca9b57aea14d2
Gé Koerkamp <ge.koerkamp@gmail.com>
2016-09-21 18:21:07 +0200

    First commit sparky build

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ Switches:
             Options for the target architecture are 'arm' or 'x86'.
   -d        Create Image for Specific Devices. Supported device names:
               all (all), pi, udoo, cuboxi, bbb, cubietruck, compulab,
-              odroidc1, odroidc2, odroidxu4
+              odroidc1, odroidc2, odroidxu4, sparky
   -v <vers> Version must be a dot separated number. Example 1.102 .
 
   -l <repo> Create docker layer. Give a Docker Repository name as the argument.
@@ -215,6 +215,11 @@ if  [ "$DEVICE" = odroidx2 ]; then
   echo 'Writing Odroid-X2 Image File'
   check_os_release "arm" $VERSION $DEVICE
   sh scripts/odroidx2image.sh -v $VERSION -p $PATCH;
+fi
+if [ "$DEVICE" = sparky ]; then
+  echo 'Writing Sparky Image File'
+  check_os_release "arm" $VERSION $DEVICE
+  sh scripts/sparkyimage.sh -v $VERSION -p $PATCH;
 fi
 if  [ "$DEVICE" = udooneo ]; then
   echo 'Writing UDOO NEO Image File'
