1fa1ec53fed6d496770298ce0933f16ef692b971
Xipmix <xipmix@users.noreply.github.com>
2017-01-26 17:34:16 +1100

    Add the script that does the work for the service

---
+++ volumio/bin/logdirs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+TAR=/etc/logdirs.tar
+
+echo "Preparing /var/log directory tree"
+ls "/var/log" 1>/dev/null || exit 1
+if [ ! -f "$TAR" ]; then
+     echo "Can't find '$TAR', skipping"
+     exit 0
+fi
+
+cd /
+tar xf "$TAR"
+
+unset TAR
+
+echo "Finalizing"
+sync
+
