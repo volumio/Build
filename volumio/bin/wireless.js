9982157e2480be6b107990395063b818d6dbe740
Earl Chew <earl_chew@yahoo.com>
2017-02-05 10:00:45 -0800

    [wireless] Rework wireless service to restart hotspot and client
    Signed-off-by: Earl Chew <earl_chew@yahoo.com>

--- volumio/bin/wireless.js
+++ volumio/bin/wireless.js
@@ -1,236 +1,389 @@
 #!/usr/local/bin/node
 
-//Volumio Network Manager - Copyright Michelangelo Guarise - Volumio.org
+let child_process = require('child_process');
+let fs = require('fs');
 
-// Time needed to settle some commands sent to the system like ifconfig
-var settleTime = 3000;
-var fs = require('fs-extra')
-var thus = require('child_process');
-var wlan = "wlan0";
-var dhcpd = "dhcpd";
-var dhclient = "/usr/bin/sudo /sbin/dhcpcd";
-var justdhclient = "/usr/bin/sudo /sbin/dhcpcd";
-var wpasupp = "wpa_supplicant -d -B -Dwext -c/etc/wpa_supplicant/wpa_supplicant.conf -i" + wlan;
-var starthostapd = "systemctl start hotspot.service";
-var stophostapd = "systemctl stop hotspot.service";
-var ifconfigHotspot = "ifconfig " + wlan + " 192.168.211.1 up";
-var ifconfigWlan = "ifconfig " + wlan + " up";
-var ifdeconfig = "sudo ip addr flush dev " + wlan + " && sudo ifconfig " + wlan + " down";
-
-function kill(process, callback) {
-	var all = process.split(" ");
-	var process = all[0];
-	var command = 'kill `pgrep -f "^' + process + '"` || true';
-	console.log("killing: " + command);
-	return thus.exec(command, callback);
-}
-
-
-
-function launch(fullprocess, name, sync, callback) {
-	if (sync) {
-		var child = thus.exec(fullprocess, {}, callback);
-		child.stdout.on('data', function(data) {
-			console.log(name + 'stdout: ' + data);
-		});
-
-		child.stderr.on('data', function(data) {
-			console.log(name + 'stderr: ' + data);
-		});
-
-		child.on('close', function(code) {
-			console.log(name + 'child process exited with code ' + code);
-		});
-	} else {
-		var all = fullprocess.split(" ");
-		var process = all[0];
-		if (all.length > 0) {
-			all.splice(0, 1);
+class Program {
+	constructor(cmd, args) {
+		console.log(`Executing program ${cmd} [ ${args} ]`)
+		let program = child_process.spawnSync(cmd, args);
+		let exitCode = program.status;
+		if ( ! exitCode) {
+			if (program.signal || program.error)
+				exitCode = -1;
 		}
-		console.log("launching " + process + " args: ");
-		console.log(all);
-		var child = thus.spawn(process, all, {});
-		child.stdout.on('data', function(data) {
-			console.log(name + 'stdout: ' + data);
-		});
+		for (let [buffer, name] of [
+			// Do not bother printing the content of stdout
+			// since it is going to be returned to the caller.
+			//
+			// [program.stdout, 'stdout'],
+			[program.stderr, 'stderr']]) {
+			let output = buffer.toString().trim();
+			for (let line of this._lines_(output))
+				console.log(`${cmd} ${name}: ${line}`)
+		}
 
-		child.stderr.on('data', function(data) {
-			console.log(name + 'stderr: ' + data);
-		});
-
-		child.on('close', function(code) {
-			console.log(name + 'child process exited with code ' + code);
-		});
-		callback();
+		this.exitCode = exitCode;
+		this.stdout = program.stdout.toString().trim();
 	}
 
-	return
-}
-
-
-function startHotspot() {
-	stopHotspot(function(err) {
-		var hotspotenabled = true;
-		try {
-			var hotspotjson = fs.readJsonSync('/data/configuration/system_controller/network/config.json', {throws: false});
-		} catch(e) {
-			console.log('First boot, starting Hotspot');
-			launch(ifconfigHotspot, "confighotspot", true, function(err) {
-			console.log("ifconfig " + err);
-			launch(starthostapd,"hotspot" , false, function() {
-				wstatus("hotspot");
-			});
-		});
-		}
-
-               if (hotspotjson != undefined && hotspotjson.enable_hotspot != undefined && hotspotjson.enable_hotspot.value != undefined && !hotspotjson.enable_hotspot.value) {
-		   			console.log('Hotspot is disabled, not starting it');
-				    launch(ifconfigWlan, "configwlanup", true, function(err) {
-				   	console.log("ifconfig " + err);
-					});
-		} else {
-
-		launch(ifconfigHotspot, "confighotspot", true, function(err) {
-			console.log("ifconfig " + err);
-			launch(starthostapd,"hotspot" , false, function() {
-				wstatus("hotspot");
-			});
-		});
-		}
-	});
-}
-
-function stopHotspot(callback) {
-	launch(stophostapd, "stophotspot" , true, function(err) {
-		launch(ifdeconfig, "ifdeconfig", true, callback);
-	});
-}
-
-function startAP(callback) {
-	console.log("Stopped hotspot (if there)..");
-	launch(ifdeconfig, "ifdeconfig", true,  function(err) {
-		console.log("Conf " + ifdeconfig);
-		launch(wpasupp, "wpa supplicant", false, function(err) {
-			console.log("wpasupp " + err);
-			wpaerr = err;
-			try {
-				dhclient = fs.readFileSync('/data/configuration/wlanstatic', 'utf8');
-				console.log("FIXED IP");
-			} catch (e) {
-				console.log("DHCP IP ");
+	* _lines_(text) {
+		let posn = 0;
+		while (text) {
+			let next = text.indexOf('\n', posn)
+			if (next == -1) {
+				yield text.substring(posn);
+				break;
 			}
-			launch(dhclient,"dhclient", false, callback);
-		});
-	});
+			yield text.substring(posn, next);
+			posn = next + 1;
+			if (posn == text.length)
+				break;
+		}
+	}
 }
 
-function stopAP(callback) {
-	kill(justdhclient, function(err) {
-		kill(wpasupp, function(err) {
+class NetworkInterface {
+	constructor(name, wlan) {
+		this.name = name;
+		this.timeout = undefined;
+		this.callback = undefined;
+		this.enabled = true;
+		this.running = false;
+		this.wlan = wlan;
+	}
+
+	destructor() {
+		this.disable();
+		if (this.running)
+			this._stopInterface();
+	}
+
+	enable() {
+		this.enabled = true;
+	}
+
+	disable() {
+		this.enabled = false;
+	}
+
+	_startInterface() {
+		console.log(`Starting ${this.name} network`);
+		this._start();
+		this.running = true;
+	}
+
+	_stopInterface() {
+		console.log(`Stopping ${this.name} network`);
+		this._stop();
+
+		let callback = this.callback;
+		clearInterval(this.timeout);
+		this.timeout = undefined;
+		this.callback = undefined;
+		this.running = false;
+
+		if (this.enabled)
 			callback();
-		});
-	});
-}
-var wpaerr;
-var lesstimer;
-var totalSecondsForConnection = 20;
-var pollingTime = 1;
-var actualTime = 0;
-var apstopped = 0
-
-function startFlow() {
-	try {
-		var wirelessjson = fs.readJsonSync('/data/configuration/system_controller/network/config.json', {throws: false});
-	} catch (e) {
-		console.log('');
 	}
-	if (wirelessjson != undefined && wirelessjson.wireless_enabled != undefined && wirelessjson.wireless_enabled.value != undefined && !wirelessjson.wireless_enabled.value) {
-		console.log('Wireless Networking DISABLED, not starting wireless flow');
-	} else {
-		console.log("Start wireless flow");
-	startAP(function () {
-		console.log("Start ap");
-		lesstimer = setInterval(function () {
-			actualTime += pollingTime;
-			if (wpaerr > 0) actualTime = totalSecondsForConnection + 1;
 
-			if (actualTime > totalSecondsForConnection) {
-				console.log("Overtime, starting plan B");
-				apstopped = 1;
-				clearTimeout(lesstimer);
-				stopAP(function () {
-					setTimeout(function () {
-						startHotspot(function () {
+	_checkPeriodically(period, callback) {
+		console.log(`Checking ${this.name} every ${period}s`);
+		this.callback = callback;
+		this.timeout = setInterval(
+			function () {
+				console.log(
+					`Checking ${this.name}`);
+				if ( ! this._check())
+					this._stopInterface();
+			}.bind(this), 1000 * period);
+	}
 
+	run(period, callback) {
+		if ( ! this.enabled) {
+			if (callback !== undefined)
+				setImmediate(callback);
+		} else {
+			this._startInterface();
+			if (callback !== undefined)
+				this._checkPeriodically(period, callback);
+		}
+	}
 
-						});
-					}, settleTime);
-				});
-			} else {
-				var iwconfig = require('wireless-tools/iwconfig');
-				var ifconfig = require('wireless-tools/ifconfig');
-				iwconfig.status(wlan, function (err, status) {
-					console.log("trying...");
-					//console.log("ssid: " status.ssid);
-					if (status.ssid != undefined) {
-						ifconfig.status(wlan, function (err, ifstatus) {
-							console.log("... joined AP, wlan0 IPv4 is " + ifstatus.ipv4_address + ", ipV6 is " + ifstatus.ipv6_address);
-							if (((ifstatus.ipv4_address != undefined) &&
-								(ifstatus.ipv4_address.length > "0.0.0.0".length))
-								||
-								((ifstatus.ipv6_address != undefined) &&
-								(ifstatus.ipv6_address.length > "::".length))) {
-								if (apstopped == 0) {
-									console.log("It's done! AP");
-									wstatus("ap");
-									clearTimeout(lesstimer);
-									restartAvahi();
-								}
-							}
-						});
-					}
-				});
+	_inUse(ipAddress) {
+                let ss = new Program(
+                        '/bin/ss', [
+                        '-n', 'state', 'established', 'src', ipAddress]);
+                console.log(`${this.name} connections `
+                        + `exit code ${ss.exitCode} `
+                        + `[ ${ss.stdout} ]`);
+                let nlPosition = ss.stdout.indexOf('\n');
+                console.log(`${this.name} active connection at ${nlPosition}`);
+		return nlPosition > 1;
+	}
+}
+
+class WifiHotspot extends NetworkInterface {
+	constructor(wlan, ipAddress, dhcpRange) {
+		super('Wifi Hotspot', wlan);
+		this.ipAddress = ipAddress;
+		this.hostIP = ipAddress.split('/')[0];
+
+		this._reset();
+
+		// Write the configuration of the DHCP and DNS services
+		// into /var/run since there is no point persisting the
+		// settings beyond the lifetime of the hotspot itself.
+
+		fs.writeFileSync(
+			'/var/run/hotspot-dnsmasq.conf', [
+			`address=/#/${this.hostIP}`,
+			`dhcp-leasefile=/var/run/dnsmasq.leases`,
+			`dhcp-range=${dhcpRange}`,
+			`dhcp-option=option:router,${this.hostIP}`,
+			`dhcp-option=option:domain-name,".local"`,
+			`dhcp-option=option:dns-server,${this.hostIP}`].join(
+				'\n') + '\n',
+			{ mode: 0o644 });
+	}
+
+	_reset() {
+		let ifconfig = new Program(
+			'/sbin/ifconfig', [
+			this.wlan,
+			'0.0.0.0', 'down']);
+		console.log(`${this.name} deconfigured `
+			+ `exit code ${ifconfig.exitCode} `
+			+ `[ ${ifconfig.stdout} ]`);
+
+		let hotspot = new Program(
+			'/bin/systemctl', [
+			'stop', 'wireless-hotspot']);
+		console.log(`${this.name} stopped `
+			+ `exit code ${hotspot.exitCode} `
+			+ `[ ${hotspot.stdout} ]`);
+	}
+
+	_start() {
+		let ifconfig = new Program(
+			'/sbin/ifconfig', [
+			this.wlan,
+			this.ipAddress, 'up']);
+		console.log(`${this.name} configured `
+			+ `exit code ${ifconfig.exitCode} `
+			+ `[ ${ifconfig.stdout} ]`);
+
+		let hotspot = new Program(
+			'/bin/systemctl', [
+			'start', 'wireless-hotspot']);
+		console.log(`${this.name} started `
+			+ `exit code ${hotspot.exitCode} `
+			+ `[ ${hotspot.stdout} ]`);
+	}
+
+	_check() {
+		// Check if there are any established stations
+		// connected to the hotspot. The hotspot can be
+		// torn down if none are found.
+
+                let hostapdcli = new Program(
+			'/usr/sbin/hostapd_cli', [
+			'-i', this.wlan, 'all_sta' ]);
+                console.log(`${this.name} connections `
+                        + `exit code ${hostapdcli.exitCode} `
+                        + `[ ${hostapdcli.stdout} ]`);
+
+		return hostapdcli.exitCode == 0
+			&& (hostapdcli.stdout || false);
+	}
+
+	_stop() {
+		this._reset();
+	}
+}
+
+class WifiClient extends NetworkInterface {
+	constructor(wlan) {
+		super('Wifi Client', wlan);
+		this._reset();
+	}
+
+	_reset() {
+		let client = new Program(
+			'/bin/systemctl', [
+			'stop', 'wireless-client']);
+		console.log(`${this.name} stopped `
+			+ `exit code ${client.exitCode} `
+			+ `[ ${client.stdout} ]`);
+
+		this.ipAddress = undefined;
+	}
+
+	_start() {
+		let client = new Program(
+			'/bin/systemctl', [
+			'start', 'wireless-client']);
+		console.log(`${this.name} started `
+			+ `exit code ${client.exitCode} `
+			+ `[ ${client.stdout} ]`);
+	}
+
+	_check() {
+		// Check if an IP address is assigned to the access point.
+		// Tear down the access point if an address could not
+		// be found. This typically means that either the
+		// access point could not be joined, or that no IP
+		// address could be leased.
+
+		let ip = new Program(
+			'/bin/ip', [
+			'-f', 'inet',
+			'addr', 'show', this.wlan]);
+		console.log(`${this.name} interface address ${ip.stdout}`);
+
+		let ipAddress = undefined;
+		for (let line of ip.stdout.split('\n')) {
+			line = line.trim();
+			if (line.startsWith('inet ')) {
+				ipAddress = line.split(' ')[1].split('/')[0];
 			}
-		}, pollingTime * 1000);
-	});
-}
-}
+		}
 
-function stop(callback) {
-	stopAP(function() {
-		stopHotspot(callback);
-	});
-}
+		if (this.ipAddress != ipAddress || ip.exitCode) {
+			this.ipAddress = ipAddress;
+			console.log(`${this.name} interface check `
+				+ `exit code ${ip.exitCode} `
+				+ `address ${this.ipAddress}`)
+		}
 
+                // Also require that there are established TCP sessions using
+                // the IP address of the access point.
 
-if (process.argv.length < 2) {
-	console.log("Use: start|stop");
-} else {
-	var args = process.argv[2];
-	console.log(args);
+		return ip.exitCode == 0
+			&& this.ipAddress
+			&& this._inUse(this.ipAddress);
+	}
 
-	switch (args) {
-		case "start":
-			console.log("Cleaning previous...");
-                        stopHotspot(function () {
-                        stopAP(function() {
-                                console.log("Stopped aP");
-                                startFlow();
-                        })});
-			break;
-		case "stop":
-			stopAP(function() {});
-			break;
-		case "test":
-			wstatus("test");
-			break;
+	_stop() {
+		this._reset();
 	}
 }
 
-function wstatus(nstatus) {
-	thus.exec("echo " + nstatus + " >/tmp/networkstatus", null);
+console.log('Initialising wireless service');
+
+let wlan = 'wlan0';
+
+// Choose an IP network for the hotspot that will not collide with any
+// properly configured production network address assignment. Use the
+// address assignment from RFC2544 since those addresses should only
+// ever be present for networks under test.
+
+let hotspotAddress = '198.18.0.1/15'; // RFC2544 to avoid address collision
+let hotspotDhcpRange = '198.18.0.2,198.18.0.100';
+
+let hotspot = new WifiHotspot(wlan, hotspotAddress, hotspotDhcpRange);
+let client = new WifiClient(wlan);
+
+let config;
+try {
+	config = require(
+		'/data/configuration/system_controller/network/config.json');
+} catch (err) {
+	if (err.code != 'MODULE_NOT_FOUND')
+		throw err;
 }
 
-function restartAvahi() {
-	//thus.exec("/bin/systemctl restart avahi-daemon");
+if (config != undefined
+	&& config.enable_hotspot != undefined
+	&& config.enable_hotspot.value != undefined) {
+
+	// Setting   Hotspot   Client
+	//
+	// false     disabled  enabled
+	// 'off'     disabled  enabled
+	// 'on'      enabled   disabled
+	// 'auto'    enabled   enabled
+	// true      enabled   enabled
+
+	let enable_hotspot = config.enable_hotspot.value;
+	if (enable_hotspot) {
+		enable_hotspot = enable_hotspot.toString().toLowerCase();
+		if (enable_hotspot === 'off')
+			enable_hotspot = false;
+		else if (enable_hotspot === 'on') {
+			console.log('Client disabled');
+			client.disable();
+		}
+	}
+
+	if ( ! enable_hotspot) {
+		console.log('Hotspot disabled');
+		hotspot.disable();
+	}
 }
+
+function runNetwork() {
+
+	// Even when running the hotspot or client only, periodically check
+	// if the established network is viable, and restart it if there
+	// are no signs of activity. Check more frequently when running
+	// both network types to reduce the time a user might see as
+	// a network outage.
+
+	let hotspotOnlyPeriod = 60;
+	let clientOnlyPeriod = 60;
+
+	let hotspotPeriod = 15;
+	let clientPeriod = 30;
+
+	function runClientOnly() {
+		client.run(clientOnlyPeriod, runClientOnly);
+	}
+
+	function runHotspotOnly() {
+		hotspot.run(hotspotOnlyPeriod, runHotspotOnly);
+	}
+
+	function runHotspotAndClient() {
+		hotspot.run(hotspotPeriod, function () {
+			client.run(clientPeriod, runHotspotAndClient);
+			});
+	}
+
+	if ( ! hotspot.enabled) {
+		console.log('Selecting client only');
+		runClientOnly();
+	} else if ( ! client.enabled) {
+		console.log('Selecting hotspot only');
+		runHotspotOnly();
+	} else {
+		console.log('Selecting hotspot and client');
+		runHotspotAndClient();
+	}
+}
+
+function stopService() {
+	console.log('');
+	console.log('Exiting wireless service');
+	hotspot.destructor();
+	client.destructor();
+}
+
+function killService(sigName) {
+	stopService();
+	process.removeAllListeners(sigName);
+	process.kill(process.pid, sigName);
+	process.exit(1);
+}
+
+// Continue running the service until there is a signal received that
+// requests the service to terminate. Signals are handled synchronously
+// with respect to other activities, ensuring that the wireless services
+// are shut down before this service exits.
+
+for (let sigName of ['SIGINT', 'SIGTERM'])
+	process.on(sigName, function () { killService(sigName); });
+setInterval(function() {}, 60000);
+
+console.log('Running wireless service');
+runNetwork();
