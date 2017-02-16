97d7c1050210d02668fde5a586572f740e7109bc
volumio <info@volumio.org>
2017-02-16 01:57:16 +0100

    fix bubbleupnp x86

--- scripts/volumioconfig.sh
+++ scripts/volumioconfig.sh
@@ -260,8 +260,11 @@ elif [ $(uname -m) = i686 ] || [ $(uname -m) = x86 ] || [ $(uname -m) = x86_64 ]
   echo "Installing Upmpdcli"
   wget http://repo.volumio.org/Packages/Upmpdcli/x86/upmpdcli_1.2.12-1_i386.deb
   wget http://repo.volumio.org/Packages/Upmpdcli/x86/libupnp6_1.6.20.jfd5-1_i386.deb
+  wget http://repo.volumio.org/Packages/Upmpdcli/x86/libupnpp3_0.15.1-1_i386.deb
+  dpkg -i libupnpp3_0.15.1-1_i386.deb
   dpkg -i libupnp6_1.6.20.jfd5-1_i386.deb
   dpkg -i upmpdcli_1.2.12-1_i386.deb
+  rm /libupnpp3_0.15.1-1_i386.deb
   rm /upmpdcli_1.2.12-1_i386.deb
   rm /libupnp6_1.6.20.jfd5-1_i386.deb
 
