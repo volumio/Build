ee544c49354b204ddaa4c1c4635028bc7bcb9bb2
macmpi <spam@ipik.org>
2017-03-26 20:01:02 +0200

    optimize name swap
    handles situation where script may be called for eth and wlan before first one is terminated.
    use one _temp_name per interface type to avoid potential collisions.

--- volumio/bin/rename_netiface0.sh
+++ volumio/bin/rename_netiface0.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# This script is called by Volumio udev rule to set/swap name to eth0 or wlan0
-# as udev renaming sometimes fails with some module drivers
-# it assumes legacy style interface names such as ethX, wlanX
+# This script is called by Volumio udev rule to set/swap name for eth0 or wlan0
+# since udev renaming sometimes fails with some module drivers
+# It assumes System uses legacy style interface names such as ethX, wlanX
 
 
 if_name=$1
@@ -9,8 +9,8 @@ if_type=${if_name%%[0-9]*}
 
 ifconfig $if_type"0" down
 ifconfig $if_name down
-ip link set dev $if_type"0" name temp_name
+ip link set dev $if_type"0" name $if_type"_temp_name"
 ip link set dev $if_name name $if_type"0"
-ip link set dev temp_name name $if_name
+ip link set dev $if_type"_temp_name" name $if_name
 ifconfig $if_name up
 ifconfig $if_type"0" up
