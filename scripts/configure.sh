5642f99371e32d33da85f50a7c0bd8d47fb3e02c
volumio <info@volumio.org>
2017-04-26 13:55:44 +0000

    plymouth default theme

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -91,6 +91,7 @@ cp volumio/bin/rename_netiface0.sh build/$BUILD/root/bin/rename_netiface0.sh
 chmod a+x build/$BUILD/root/bin/rename_netiface0.sh
 #Plymouth
 cp volumio/usr/*  build/$BUILD/usr/
+plymouth-set-default-theme volumio
 echo 'Done Copying Custom Volumio System Files'
 
 echo "Stripping binaries and libraries to save space"
