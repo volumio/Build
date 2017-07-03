33c96b2e1b668c1c417458d6f85e0e58fe629293
macmpi <spam@ipik.org>
2017-07-03 16:46:49 +0200

    superfluous apt-get update

--- scripts/cuboxiconfig.sh
+++ scripts/cuboxiconfig.sh
@@ -61,7 +61,6 @@ echo "(otherwise cuboxi may not boot due to size of initrd)"
 sed -i "s/MODULES=most/MODULES=dep/g" /etc/initramfs-tools/initramfs.conf
 
 echo "Installing winbind here, since it freezes networking"
-apt-get update
 apt-get install -y winbind libnss-winbind
 
 echo "Cleaning APT Cache and remove policy file"
