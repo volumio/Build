93df99e7542310517262579f08e130017d57a087
Paul Kendall <pkendall64@gmail.com>
2018-02-04 11:48:35 +1300

    Make the orangepi boot partition vfat

--- scripts/orangepiconfig.sh
+++ scripts/orangepiconfig.sh
@@ -8,8 +8,7 @@ echo "Creating \"fstab\""
 echo "# OrangePi fstab" > /etc/fstab
 echo "" >> /etc/fstab
 echo "proc            /proc           proc    defaults        0       0
-# /dev/mmcblk0p1  /boot           vfat    defaults,utf8,user,rw,umask=111,dmask=000        0       1
-/dev/mmcblk0p1  /boot           ext4    defaults
+/dev/mmcblk0p1  /boot           vfat    defaults,utf8,user,rw,umask=111,dmask=000        0       1
 tmpfs   /var/log                tmpfs   size=20M,nodev,uid=1000,mode=0777,gid=4, 0 0
 tmpfs   /var/spool/cups         tmpfs   defaults,noatime,mode=0755 0 0
 tmpfs   /var/spool/cups/tmp     tmpfs   defaults,noatime,mode=0755 0 0
