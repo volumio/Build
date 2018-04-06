5751a3358ba1136679bf2a2301ddfb400ddef425
Sergio Visinoni <piffio@piffio.org>
2018-04-06 23:52:12 +0200

    Add an initial version of a docker build script

---
+++ docker-build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Docker based image builder
+#
+# This is an alternative method for building a volumio image that does not
+# require the user to have a debian system with specific packages versions.
+#
+# This method relies on a pre-built docker container that will perform the
+# whole build process and generate the required image in the local directory.
+
+DOCKER="docker"
+
+DPATH=$(which "${DOCKER}" 2> /dev/null)
+FOUND=$?
+RUNDATE=$(date -I)
+
+if [ "${FOUND}" != "0" ]; then
+	echo "You need to have ${DOCKER} installed to be able to perform a Docker based build"
+	exit 1
+fi
+
+if [ $# -lt 2 ]; then
+	echo "Missign build parameters!"
+	echo
+	echo "This script is a wrapper around the main ./build.sh script, and as such it requires specific parameters"
+	echo "to be passed over to build.sh."
+	echo
+	echo "Please run the command ./build.sh without parameters for a full help."
+	exit 2
+fi
+
+${DOCKER} run --name=volumio-build --privileged --tty \
+	-v ${PWD}:/build piffio/volumio-build \
+	$@ | tee docker-build-${RUNDATE}.log
--- docker-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --privileged --tty -v ${PWD}:/build piffio/volumio-build -d pi -b arm -v 3.001
