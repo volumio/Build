2737c2eed6bf7aff96c45fd389593ae313d798fe
volumio <info@volumio.org>
2018-07-05 00:06:51 +0200

    Proper hash creation

--- scripts/finalize.sh
+++ scripts/finalize.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Computing Volumio folder Hash Checksum"
-HASH=`/usr/bin/find /volumio -type f | sort -u | xargs cat | md5sum | tr -d "-" | tr -d " \t\n\r"`
-echo "VOLUMIO_HASH=${HASH}" >> /mnt/volumio/rootfs/etc/os-release
+HASH=`/usr/bin/md5deep -r -l -s -q /mnt/volumio/rootfs/volumio | sort | md5sum | tr -d "-" | tr -d " \t\n\r"`
+echo "VOLUMIO_HASH=\"${HASH}\"" >> /mnt/volumio/rootfs/etc/os-release
 
