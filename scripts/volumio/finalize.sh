73df1804c8576e522366ee8da7819d3e07420f36
ashthespy <ashthespy@users.noreply.github.com>
2020-02-18 17:55:23 +0100

    Fix `finalize.sh`

--- scripts/volumio/finalize.sh
+++ scripts/volumio/finalize.sh
@@ -1,44 +1,41 @@
 #!/bin/bash
 
-HARDWARE=`/bin/cat /mnt/volumio/rootfs/etc/os-release | grep "VOLUMIO_HARDWARE" | cut -d \\" -f2 | tr -d "\n"`
-BASEDIR=/mnt/volumio/rootfs
+set -eo pipefail
+
+# HARDWARE=`/bin/cat /mnt/volumio/rootfs/etc/os-release | grep "VOLUMIO_HARDWARE" | cut -d \\" -f2 | tr -d "\n"`
+
+# ROOTFSMNT=/mnt/volumio/rootfs
 log "Computing Volumio folder Hash Checksum" "info"
-HASH=`/usr/bin/md5deep -r -l -s -q /mnt/volumio/rootfs/volumio | sort | md5sum | tr -d "-" | tr -d " \t\n\r"`
-log "VOLUMIO_HASH=\"${HASH}\"" >> /mnt/volumio/rootfs/etc/os-release
 
-log "Cleanup to save space" "info"
+HASH="$(md5deep -r -l -s -q ${ROOTFSMNT}/volumio | sort | md5sum | awk '{print $1}')"
+log "HASH: ${HASH}" "dbg"
+# HASH=`/usr/bin/md5deep -r -l -s -q ${ROOTFSMNT}/volumio | sort | md5sum | tr -d "-" | tr -d " \t\n\r"`
+cat <<-EOF >> ${ROOTFSMNT}/etc/os-release
+VOLUMIO_HASH="${HASH}"
+EOF
 
+log "Cleaning stuff to save space" "info"
 log "Cleaning docs"
-find /mnt/volumio/rootfs/usr/share/doc -depth -type f ! -name copyright|xargs rm || true
-find /mnt/volumio/rootfs/usr/share/doc -empty|xargs rmdir || true
+find ${ROOTFSMNT}/usr/share/doc -depth -type f ! -name copyright -delete #| xargs rm
+find ${ROOTFSMNT}/usr/share/doc -empty -delete #|xargs rmdir || true
 
-if [ $HARDWARE != "x86" ]; then
-
-log "Cleaning man and caches"
-rm -rf /mnt/volumio/rootfs/usr/share/man/* /mnt/volumio/rootfs/usr/share/groff/* /mnt/volumio/rootfs/usr/share/info/*
-rm -rf /mnt/volumio/rootfs/usr/share/lintian/* /mnt/volumio/rootfs/usr/share/linda/* /mnt/volumio/rootfs/var/cache/man/*
-
-log "Stripping binaries"
-
-
-STRP_DIRECTORIES=("$BASEDIR/lib/" "$BASEDIR/bin/" "$BASEDIR/usr/sbin" "$BASEDIR/usr/local/bin/")
-for DIR in "${STRP_DIRECTORIES[@]}"; do
- log "$DIR Pre strip size " "$(du -sh0 "$DIR" | awk '{print $1}')"
- find "$DIR" -type f  -exec strip --strip-all > /dev/null 2>&1 {} ';'
- log "$DIR Post strip size " "$(du -sh0 "$DIR" | awk '{print $1}')"
-done
-
-
-find $BASEDIR/lib/ -type f  -exec strip --strip-all > /dev/null 2>&1 {} ';'
-find $BASEDIR/bin/ -type f  -exec strip --strip-all > /dev/null 2>&1 {} ';'
-find $BASEDIR/usr/sbin -type f  -exec strip --strip-all > /dev/null 2>&1 {} ';'
-find $BASEDIR/usr/local/bin/ -type f  -exec strip --strip-all > /dev/null 2>&1 {} ';'
+if [[ $BUILD != "x86" ]]; then
+  log "Cleaning man and caches"
+  rm -rf ${ROOTFSMNT}/usr/share/man/* ${ROOTFSMNT}/usr/share/groff/* ${ROOTFSMNT}/usr/share/info/*
+  rm -rf ${ROOTFSMNT}/usr/share/lintian/* ${ROOTFSMNT}/usr/share/linda/* ${ROOTFSMNT}/var/cache/man/*
 
 else
   echo "X86 Environmant detected, not cleaning"
-  find $BASEDIR/bin/ -type f  -exec strip --strip-all > /dev/null 2>&1 {} ';'
-find $BASEDIR/usr/sbin -type f  -exec strip --strip-all > /dev/null 2>&1 {} ';'
-find $BASEDIR/usr/local/bin/ -type f  -exec strip --strip-all > /dev/null 2>&1 {} ';'
-
-
 fi
+
+log "Stripping binaries"
+STRP_DIRECTORIES=("${ROOTFSMNT}/lib/" \
+                  "${ROOTFSMNT}/bin/" \
+                  "${ROOTFSMNT}/usr/sbin" \
+                  "${ROOTFSMNT}/usr/local/bin/")
+
+for DIR in "${STRP_DIRECTORIES[@]}"; do
+  log "$DIR Pre strip size " "$(du -sh0 "$DIR" | awk '{print $1}')"
+  find "$DIR" -type f  -exec bash -c 'strip --strip-all > /dev/null 2>&1' {} ';'
+  log "$DIR Post strip size " "$(du -sh0 "$DIR" | awk '{print $1}')"
+done
