11645d2e1d479ae21ea11882a2f0ef64184ade12
ashthespy <ashthespy@users.noreply.github.com>
2020-02-20 13:50:07 +0100

    Add lesbonscomptes keys

--- scripts/config.sh
+++ scripts/config.sh
@@ -2,7 +2,11 @@
 # Central location for Build System configuration
 
 declare -A SecureApt=(
-  [nodesource.gpg]="https://deb.nodesource.com/gpgkey/nodesource.gpg.key"  \
-    [debian_10.gpg]="https://ftp-master.debian.org/keys/archive-key-10.asc" \
+  [debian_10.gpg]="https://ftp-master.debian.org/keys/archive-key-10.asc" \
+    [nodesource.gpg]="https://deb.nodesource.com/gpgkey/nodesource.gpg.key"  \
+    [lesbonscomptes.gpg]="https://www.lesbonscomptes.com/pages/jf-at-dockes.org.pgp" \
+    #TODO Not needed for arm64 and x86
     [raspbian.gpg]="https://archive.raspbian.org/raspbian.public.key" \
   )
+
+export SecureApt
