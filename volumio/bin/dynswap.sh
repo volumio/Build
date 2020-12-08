0120ed1e9b82f10bd3efe5f8510299d8a6def330
ashthespy <ashthespy@users.noreply.github.com>
2020-12-08 17:11:06 +0100

    Fix swap on newer kernels

--- volumio/bin/dynswap.sh
+++ volumio/bin/dynswap.sh
@@ -1,22 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-RAMSIZE=`cat /proc/meminfo | grep MemTotal | awk '{ print $2 }'`
-SWAPDEVS=`cat /proc/swaps | wc -l`
+RAMSIZE=$(grep MemTotal /proc/meminfo | awk '{ print $2 }')
+SWAPDEVS=$(wc -l </proc/swaps)
+SWAPDIR="/swap"
+SWAPFILE="${SWAPDIR}/swapfile"
 
-if [ ${RAMSIZE} -le 512844 -a ${SWAPDEVS} -le 1 ]; then
+if [[ "${RAMSIZE}" -le 512844 ]] && [[ ${SWAPDEVS} -le 1 ]]; then
     echo "512 MB or less RAM Detected, need to enable swap"
-    if [ ! -e /data/swapfile ]; then
-	echo "No Swapfile present, creating it..."
-	fallocate -l 512M /data/swapfile
-	echo "Securing Swap permissions"
-	chown root:root /data/swapfile
-	chmod 0600 /data/swapfile
-	echo "Preparing SwapFile"
-	mkswap /data/swapfile
+    [[ -d ${SWAPDIR} ]] || mkdir -m 700 ${SWAPDIR}
+    mount -L volumio_data ${SWAPDIR}
+    if [ ! -e ${SWAPFILE} ]; then
+        echo "No Swapfile present, creating it..."
+        fallocate -l 512M ${SWAPFILE}
+        echo "Securing Swap permissions"
+        chown root:root ${SWAPFILE}
+        chmod 0600 ${SWAPFILE}
+        echo "Preparing SwapFile"
+        mkswap ${SWAPFILE}
     fi
-	
+
     echo "Enabling Swap"
-    swapon /data/swapfile
+    swapon ${SWAPFILE}
     echo "Setting swappiness to 40"
     sysctl vm.swappiness=40
 fi
