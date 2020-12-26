f38cb3985aa7581e6c5380c6133522d2971594c0
ashthespy <ashthespy@users.noreply.github.com>
2020-12-26 17:38:04 +0100

    [x86] Tweak kiosk mode, remove hardcoded timeouts, add logging

--- scripts/volumio/install-kiosk_x86.sh
+++ scripts/volumio/install-kiosk_x86.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2034
 ## Kioskmode setup for x86 devices
-
+#TODO: Combine this with regular kiosk scripts?
 CMP_NAME=volumio-kiosk-x86
 
 #shellcheck source=/dev/null
@@ -12,8 +11,8 @@ CMP_PACKAGES=(
   # Keyboard config
   "keyboard-configuration"
   # Display stuff
-  "openbox" "unclutter" "xorg" "xinit" "libexif12"
-  # Browser
+  "openbox" "unclutter" "xorg" "xinit"
+  # Browser # TODO: Why not firefox?
   "chromium" "chromium-l10n"
   # Fonts
   "fonts-arphic-ukai" "fonts-arphic-gbsn00lp" "fonts-unfonts-core"
@@ -29,48 +28,56 @@ mkdir /data/volumiokiosk
 
 cat <<-EOF >/opt/volumiokiosk.sh
 #!/usr/bin/env bash
-#set -eo pipefail
+set -eo pipefail
+exec >"/var/log/volumiokiosk.log" 2>&1
+# Wait for Volumio webUI to be available 
+while true; do timeout 3 bash -c "</dev/tcp/127.0.0.1/3000" >/dev/null 2>&1 && break; done
 export DISPLAY=:0
 xset -dpms
 xset s off
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /data/volumiokiosk/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' /data/volumiokiosk/Default/Preferences
+[[ -e /data/volumiokiosk/Default/Preferences ]] && {
+  sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /data/volumiokiosk/Default/Preferences
+  sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' /data/volumiokiosk/Default/Preferences
+}
 openbox-session &
 while true; do
-  rm -rf ~/.{config,cache}/chromium/
-  /usr/bin/chromium \\
-		--kiosk \\
-		--touch-events \\
-		--disable-touch-drag-drop \\
-		--disable-overlay-scrollbar \\
-		--enable-touchview \\
-		--enable-pinch \\
-		--window-size=800,480 \\
-		--window-position=0,0 \\
-		--disable-session-crashed-bubble \\
-		--disable-infobars \\
-		--no-first-run \\
-		--no-sandbox \\
-		--user-data-dir='/data/volumiokiosk' \\
-		--disable-translate \\
-		--show-component-extension-options \\
-		--ignore-gpu-blacklist \\
-		--disable-background-networking \\
-		--use-gl=egl \\ 
-		--enable-remote-extensions \\
-		--enable-native-gpu-memory-buffers \\
-		--disable-quic \\
-		--enable-fast-unload \\
-		--enable-tcp-fast-open \\
-		--disable-gpu-compositing \\
-		--force-gpu-rasterization \\
-		--enable-zero-copy \\
-		'http://localhost:3000'
+  # TODO: Why not just launch a private window?
+  rm -rf ~/.{config,cache}/chromium/ || echo "No {config,cache} folders to remove"
+  /usr/bin/chromium \
+    --kiosk \
+    --touch-events \
+    --disable-touch-drag-drop \
+    --disable-overlay-scrollbar \
+    --enable-touchview \
+    --enable-pinch \
+    --window-size=800,480 \
+    --window-position=0,0 \
+    --disable-session-crashed-bubble \
+    --disable-infobars \
+    --no-first-run \
+    --no-sandbox \
+    --user-data-dir='/data/volumiokiosk' \
+    --disable-translate \
+    --show-component-extension-options \
+    --ignore-gpu-blacklist \
+    --disable-background-networking \
+    --use-gl=egl \
+    --enable-remote-extensions \
+    --enable-native-gpu-memory-buffers \
+    --disable-quic \
+    --enable-fast-unload \
+    --enable-tcp-fast-open \
+    --disable-gpu-compositing \
+    --force-gpu-rasterization \
+    --enable-zero-copy \
+    'http://localhost:3000'
 done
 EOF
 chmod +x /opt/volumiokiosk.sh
 
 log "Creating Systemd Unit for ${CMP_NAME}"
+#TODO: Kiosk should be launched by a Kiosk user and not by root!?
+#usermod -aG tty,video kiosk
 cat <<-EOF >/lib/systemd/system/volumio-kiosk.service
 [Unit]
 Description=Start Volumio Kiosk
@@ -78,41 +85,12 @@ Wants=volumio.service
 After=volumio.service
 [Service]
 Type=simple
-User=volumio
-Group=audio
-ExecStart=/usr/bin/startx /etc/X11/Xsession /opt/volumiokiosk.sh 
-# Give a reasonable amount of time for the server to start up/shut down
-TimeoutSec=300
+User=root
+Group=root
+ExecStart=/usr/bin/startx /etc/X11/Xsession /opt/volumiokiosk.sh -- -keeptty
 [Install]
 WantedBy=multi-user.target
 EOF
 
 log "Enabling ${CMP_NAME} service"
 ln -s /lib/systemd/system/volumio-kiosk.service /etc/systemd/system/multi-user.target.wants/volumio-kiosk.service
-
-log "Allowing volumio to start an Xsession"
-sed -i "s/allowed_users=console/allowed_users=anybody/" /etc/X11/Xwrapper.config
-
-log "Hiding mouse cursor"
-cat <<-'EOF' >/root/.xinitrc
-#!/bin/sh
-
-if [ -d /etc/X11/xinit/xinitrc.d ]; then
-  for f in /etc/X11/xinit/xinitrc.d/*; do
-    [ -x "$f" ] && . "$f"
-  done
-  unset f
-fi
-
-xrdb -merge ~/.Xresources         # aggiorna x resources db
-
-#xscreensaver -no-splash &         # avvia il demone di xscreensaver
-xsetroot -cursor_name left_ptr &  # setta il cursore di X
-#sh ~/.fehbg &                     # setta lo sfondo con feh
-
-exec openbox-session              # avvia il window manager
-
-exec unclutter &
-EOF
-
-
